ae329628811434697aa5c5a1c27f667fdcd206a5
Everton Almeida <everton.o.almeida@outlook.com>
2019-04-03 19:09:58 +0100

    changed forencast.js to show real feel, max and min temperature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,13 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined )
         } else {
-            callback(undefined, body.daily.data[0].summary +' It is corrently ' + body.currently.temperature +' degrees out. There is a ' + body.currently.precipProbability + "% chance of rain.")
+            callback(undefined, 
+                body.daily.data[0].summary +' It is corrently ' + 
+                body.currently.temperature +' degrees out. There is a ' + 
+                body.currently.precipProbability + "% chance of rain. Feels Like: " +
+                body.currently.apparentTemperature + " Low: " +
+                body.daily.data[0].temperatureLow + " High: " +
+                body.daily.data[0].temperatureHigh)
         }
     })
 }
